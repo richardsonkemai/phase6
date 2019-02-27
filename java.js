e82a7fab72562881f7c71b6a6c98c8ec3fd76a99
richardsonkemai <46830623+richardsonkemai@users.noreply.github.com>
2019-02-27 13:36:49 -0800

    Add files via upload

--- java.js
+++ java.js
@@ -1,20 +1,21 @@
 function newImage(el){
-	if (el.src.match("images/chinese_e.jpg")){
-	el.src = "images/chinese_s.jpg";
+	if (el.src.match("images/chinese_e.png")){
+	el.src = "images/chinese_s.png";
 
-} else if (el.src.match("images/chinese_s.jpg")){
-	el.src = "images/chinese_c.jpg";
+} else if (el.src.match("images/chinese_s.png")){
+	el.src = "images/chinese_c.png";
  
-} else if (el.src.match("images/chinese_c.jpg")){
-	 el.src = "images/chinese_a.jpg";
+} else if (el.src.match("images/chinese_c.png")){
+	 el.src = "images/chinese_a.png";
 
-} else if (el.src.match("images/chinese_a.jpg")){
-	 el.src = "images/chinese_p.jpg";
+} else if (el.src.match("images/chinese_a.png")){
+	 el.src = "images/chinese_p.png";
 	 
-} else if (el.src.match("images/chinese_p.jpg")){
-	 el.src = "images/chinese_e.jpg";
+} else if (el.src.match("images/chinese_p.png")){
+	 el.src = "images/chinese_e.png";
  
  }else {
 	
 }
 }
+
